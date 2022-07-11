b75c2b1538d518dc62d14a9623cd8bd380eab650
Cyrine-Belhassen <sirine.belhassen1@esprit.tn>
2022-07-11 20:20:01 +0100

    template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserTemplateComponent} from "./user-template/user-template.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserTemplateComponent,
+    children:[
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserTemplateComponent } from './user-template/user-template.component';
+import { UserHeaderComponent } from './user-header/user-header.component';
+import { UserFooterComponent } from './user-footer/user-footer.component';
+import { UserBodyComponent } from './user-body/user-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserTemplateComponent,
+    UserHeaderComponent,
+    UserFooterComponent,
+    UserBodyComponent
   ],
   imports: [
     BrowserModule,
